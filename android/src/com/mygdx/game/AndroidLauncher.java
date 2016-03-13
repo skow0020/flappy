0acda626792a03e13c62761aa70a40e86eb50f8c
Colin Skow <skow0020@gmail.com>
2016-03-13 11:33:09 -0500

    commit

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -5,12 +5,25 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.flappy;
+import com.google.android.gms.ads.*;
 
 public class AndroidLauncher extends AndroidApplication {
+	private AdView adView;
+	private AdRequest adRequest;
+	private static final String AD_UNIT_ID_BANNER = "ca-app-pub-3940256099942544/6300978111";
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new flappy(), config);
+
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		adView.setAdUnitId(AD_UNIT_ID_BANNER);
+		adRequest = new AdRequest.Builder().build();
+
+		adView.loadAd(adRequest);
+		String x = "";
 	}
 }
